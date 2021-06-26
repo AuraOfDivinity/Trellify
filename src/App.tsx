976ef2b156a16f60adb160243154db133cae2362
Asel <peirisasel373@Gmail.com>
2021-06-26 23:10:14 +0530

    Column and Card Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { AppContainer } from './styles'
+import { Column } from './Column'
+import { Card } from './Card'
 
 function App() {
   return (
     <AppContainer>
-      Columns will go here.
+      <Column text="To Do">
+        <Card text="Develop the search bar,"></Card>
+      </Column>
+      <Column text="In Progress">
+        <Card text="Develop the home page."></Card>
+      </Column>
+      <Column text="Done">
+        <Card text="Design the application."></Card>
+      </Column>
     </AppContainer>
   );
 }
